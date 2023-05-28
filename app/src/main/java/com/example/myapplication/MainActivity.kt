bc6b5830d0baecff050505584a4a3cc68332816b
Allyson Luan Dunke <allyson.dunke@gmail.com>
2023-05-27 21:17:33 -0300

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var operand1: Double = 0.0
+    private var operator: String? = null
+    private var clearInput: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Achei no Google para ocultar a barra de status do Android
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Definir os listeners para os botões numéricos
+        binding.button0.setOnClickListener { numberPressed("0") }
+        binding.button1.setOnClickListener { numberPressed("1") }
+        binding.button2.setOnClickListener { numberPressed("2") }
+        binding.button3.setOnClickListener { numberPressed("3") }
+        binding.button4.setOnClickListener { numberPressed("4") }
+        binding.button5.setOnClickListener { numberPressed("5") }
+        binding.button6.setOnClickListener { numberPressed("6") }
+        binding.button7.setOnClickListener { numberPressed("7") }
+        binding.button8.setOnClickListener { numberPressed("8") }
+        binding.button9.setOnClickListener { numberPressed("9") }
+
+        // Definir os listeners para os botões de operação
+        binding.buttonAdd.setOnClickListener { operatorPressed("+") }
+        binding.buttonSubtract.setOnClickListener { operatorPressed("-") }
+        binding.buttonMultiply.setOnClickListener { operatorPressed("*") }
+        binding.buttonDivide.setOnClickListener { operatorPressed("/") }
+
+        // Definir o listener para o botão de vírgula
+        binding.buttonDot.setOnClickListener { dotPressed() }
+
+        // Definir o listener para o botão de igual
+        binding.buttonEquals.setOnClickListener { calculateResult() }
+
+        // Definir o listener para o botão de limpar
+        binding.buttonClear.setOnClickListener { clearCalculator() }
+    }
+
+    private fun numberPressed(number: String) {
+        var currentInput = binding.textViewResult.text.toString()
+        if(operator != null && clearInput) {
+            currentInput = ""
+        }
+        binding.textViewResult.text = currentInput + number
+        clearInput = false
+    }
+
+    private fun dotPressed() {
+        val currentInput = binding.textViewResult.text.toString()
+        if (!currentInput.contains(".")) {
+            binding.textViewResult.text = currentInput + "."
+        }
+    }
+
+    private fun operatorPressed(operator: String) {
+        // Verifica se já houve uma operação antes
+        if(binding.textViewInput.text.isNotEmpty()) {
+            calculateResult()
+        }
+
+        val currentInput = binding.textViewResult.text.toString()
+        operand1 = binding.textViewResult.text.toString().toDouble()
+        this.operator = operator
+        clearInput = true
+
+        binding.textViewInput.text = currentInput + operator
+    }
+
+    private fun calculateResult() {
+        val operand2 = binding.textViewResult.text.toString().toDouble()
+        var result: Double? = null
+
+        when (operator) {
+            "+" -> result = operand1 + operand2
+            "-" -> result = operand1 - operand2
+            "*" -> result = operand1 * operand2
+            "/" -> result = operand1 / operand2
+        }
+
+        if (result != null) {
+            binding.textViewResult.text = result.toString().removeSuffix(".0")
+        }
+
+        binding.textViewInput.text = ""
+        operand1 = 0.0
+        operator = null
+        clearInput = true
+    }
+
+    private fun clearCalculator() {
+        binding.textViewInput.text = ""
+        binding.textViewResult.text = ""
+        operand1 = 0.0
+        operator = null
+        clearInput = false
+    }
+}
